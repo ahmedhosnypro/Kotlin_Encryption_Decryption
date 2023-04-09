2d2b2b13d6709602d9fc3c90ad47223c24f47293
Ahmed Hosny <ahhosnyas@gmail.com>
2023-04-09 16:32:58 +0200

    Stage 1/6: Encrypted!

--- Encryption-Decryption (Kotlin)/task/src/Main.kt
+++ Encryption-Decryption (Kotlin)/task/src/Main.kt
@@ -1,5 +1,22 @@
 package encryptdecrypt
 
 fun main() {
-    println("Hello World!")
+    val s = "we found a treasure!"
+    val sb = StringBuilder()
+    for (element in s) {
+        var newChar = element
+        if (element.code in 65..90) {
+            val n = 90 - element.code
+            newChar = if (n < 13) {
+                (90 + n).toChar()
+            } else (65 - (25 - n)).toChar()
+        } else if (element.code in 97..122) {
+            val n = 122 - element.code
+            newChar = if (n < 13) {
+                (97 + n).toChar()
+            } else (122 - (25 - n)).toChar()
+        }
+        sb.append(newChar)
+    }
+    println(sb)
 }
